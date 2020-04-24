367a08c36a9268951a2ad1c8ddbbdf933add23d7
Guillaume <guillaume.briard@gmail.com>
2020-04-25 00:28:20 +0200

    CP and template for evenement

--- inc/ct_and_cpt.php
+++ inc/ct_and_cpt.php
@@ -40,5 +40,40 @@ function custom_tax_cpt() {
 	register_post_type( 'slider', $args_slider ); // register CP
 	
 	// slider taxonomy and CPT end
+
+	// Evenement taxonomy and CPT end
+
+	$labels_event = [
+		'name'				 => __( 'Evènements' ),
+		'singular_name' 	 => __( 'Evènement' ),
+		'add_new'            => __( 'Ajouter nouveau'),
+		'add_new_item'       => __( 'Nouvel évènement'),
+		'edit_item'          => __( 'Editer évènement'),
+		'new_item'           => __( 'Nouveau évènement'),
+		'all_items'          => __( 'Tous les évènements'),
+		'view_item'          => __( 'Voir évènement'),
+		'search_items'       => __( 'Rechercher évènements'),
+		'not_found'          =>  __( 'Aucun évènement trouvé'),
+		'not_found_in_trash' => __( 'Aucun slévènementider trouvé dans la corbeille'), 
+		'parent_item_colon'  => '',
+		'menu_name'          => __( 'Evènement')
+	];
+
+	$args_event = [
+		'labels'             => $labels_event,
+		'public' 			 => true,
+		'has_archive' 		 => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true, 
+		'show_in_menu'       => true, 
+		'query_var'          => true,
+		'has_archive'        => true,
+		'rewrite' 			 => array('slug' => 'evenements'),
+		'menu_position'      => 4,
+		'menu_icon'			 => 'dashicons-calendar-alt',
+		'supports' => array( 'title', 'custom-fields', 'thumbnail', 'excerpt' ),
+	];
+
+	register_post_type( 'yatchsevenement', $args_event ); // register CP
 }
 add_action( 'init', 'custom_tax_cpt' );
