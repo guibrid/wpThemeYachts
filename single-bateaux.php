3e2014db0b6881849eae4cfd998c5897b58b1ee7
Guillaume <guillaume.briard@gmail.com>
2020-05-04 05:54:29 +0200

    bapteau page layout update

--- single-bateaux.php
+++ single-bateaux.php
@@ -44,7 +44,7 @@ get_header();
 
                     <?php if( acf_photo_gallery('bateau_galerie', $post->ID) ) : ?>
                     <div class="row" data-aos="zoom-in"><!-- Slider -->
-                        <div class="col-12">
+                        <div class="col-xl-8 offset-xl-2">
                             <?php $images = acf_photo_gallery('bateau_galerie', $post->ID);
 
                             if( count($images) ){ ?>
@@ -70,7 +70,7 @@ get_header();
 
                     <?php if( get_field( "bateau_video" ) ) : ?>
                     <div class="row">
-                        <div class="col-12 video" data-aos="zoom-in">
+                        <div class="col-xl-8 offset-xl-2 video" data-aos="zoom-in">
                             <iframe src="<?php echo get_field( "bateau_video" ); ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                         </div>
                     </div>
@@ -85,7 +85,7 @@ get_header();
                         <div class="col-12" data-aos="zoom-in">
                             <div class="text-center title"><img src="<?php echo get_bloginfo('stylesheet_directory'); ?>/img/configuration-title.gif" /></div>
                         </div>
-                        <div class="col-12" data-aos="zoom-in">
+                        <div class="col-xl-8 offset-xl-2" data-aos="zoom-in">
                             <div class="text-center imgPlan"><img src="<?php echo get_field( "plan_du_bateau" )['url']; ?>" class="img-fluid" /></div>
                             <p class="text-center"><?php if (get_field( "plan_du_bateau" )) { ?><a target="_blank" href="<?php echo get_field( "bateau_lien_plan" )['url']; ?>" class="customButton blackButton">Télécharger le pdf <i class="fas fa-chevron-right fa-xs"></i></a><?php } ?></p>
                         </div>
