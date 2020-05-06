09e62133adace25c8b86d666f806c5a9f549fca8
Guillaume <guillaume.briard@gmail.com>
2020-05-06 10:02:46 +0200

    add link to message block

--- template-parts/content-sliderNews.php
+++ template-parts/content-sliderNews.php
@@ -10,9 +10,16 @@ $my_query2 = new WP_Query( $args2 );
 if ($my_query2->have_posts()) {
 
     while ( $my_query2->have_posts() ) : $my_query2->the_post();
-
-        echo '<div class="newsblock"><div class="news"> <p>'.get_the_title().'</p>
-        <p><b>contact@lesyatchsdelyon.com<br />04 72 56 51 23</b></p></div></div>';
+var_dump(get_field('message_lien'));
+        echo '
+        <div class="newsblock">
+            <div class="news">';
+            if (get_field('message_lien')){
+                echo '<p><a href="'.get_field('message_lien').'">'.get_the_title().'</a></p>';
+            } else {
+                echo '<p>'.get_the_title().'</p>';
+            };
+        echo '<p><b>contact@lesyatchsdelyon.com<br />04 72 56 51 23</b></p></div></div>';
 
     endwhile; 
     
