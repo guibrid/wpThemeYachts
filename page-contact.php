835b2b8abcd4bc227d781e0f2ffc9a31e2071675
Guillaume <guillaume.briard@gmail.com>
2020-05-14 19:26:52 +0200

    fix typos

--- page-contact.php
+++ page-contact.php
@@ -17,19 +17,18 @@ get_header();
 
         <div class="row">
 
-            <div class="col-sm-6 order-sm-12" id="contactContent">
+            <div class="col-sm-6" id="contactForm">
+                <h3>Demande de devis</h3>
+                <?php echo do_shortcode('[contact-form-7 id="20" title="Formulaire de contact"]'); ?>
+            </div>
+
+            <div class="col-sm-6" id="contactContent">
                 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-                <?php the_content(); ?>
+                    <?php the_content(); ?>
 
                 <?php endwhile;  endif; ?>
                 <iframe width="100%" scrolling="no" height="350" frameborder="0" src="http://maps.google.fr/maps?f=q&source=s_q&hl=fr&geocode=&q=15+Quai+Rambaud,+69002+Lyon,+Rh%C3%B4ne,+Rh%C3%B4ne-Alpes&sll=45.757643,4.822655&sspn=0.036769,0.104713&g=13+Quai+Rambaud,+69002+Lyon,+Rh%C3%B4ne,+Rh%C3%B4ne-Alpes&ie=UTF8&ll=45.754948,4.820595&spn=0.018388,0.052357&z=14&output=embed&s=AARTsJpzE5f1FpQwSOuHcX5YT2IfIslInQ" marginwidth="0" marginheight="0"></iframe>
-            </div>
-
-            <div class="col-sm-6 order-sm-1" id="contactForm">
-                    <h3>Demande de devis?</h3>
-                <?php echo do_shortcode('[contact-form-7 id="20" title="Formulaire de contact"]'); ?>
-            
             </div>
 
         </div>
