5630d14340b95da460973046fd397bfee7f0ea87
Guillaume <guillaume.briard@gmail.com>
2020-05-28 13:50:53 +0200

    Slider prev et next

--- template-parts/bateau-galery.php
+++ template-parts/bateau-galery.php
@@ -16,6 +16,14 @@
                     </div>
                 <?php }  ?>
             </div>
+            <a class="carousel-control-prev" href="#bateauCarousel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#bateauCarousel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
         </div>
     <?php  } ?>
 <?php endif; ?>
