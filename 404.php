b87df0b1ee1876017c6effb00899bf13389056e7
Guillaume <guillaume.briard@gmail.com>
2020-05-31 12:10:27 +0200

    404 PAGE

--- 404.php
+++ 404.php
@@ -14,45 +14,23 @@ get_header();
 		<main id="main" class="site-main">
 
 			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'yatchslyon' ); ?></h1>
-				</header><!-- .page-header -->
+				<div class="container">
+					<div class="row">
+						<div class="col text-center">
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'yatchslyon' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'yatchslyon' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$yatchslyon_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'yatchslyon' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$yatchslyon_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
+						<?php
+                    if (ICL_LANGUAGE_CODE == "fr") {
+                        $img404 = '404-fr.gif';
+                    } else if (ICL_LANGUAGE_CODE == "es") {
+                        $img404 = '404-es.gif';
+                    } else {
+						$img404 = '404-en.gif';
+					}
+                    ?>
+                <img src="<?php echo get_template_directory_uri() . '/img/'.$img404; ?>" class="img-fluid" style="margin:40px 0;" />
+						</div>
+					</div>
+				</div>
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
