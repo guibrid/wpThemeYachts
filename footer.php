f96df932f60ccbc77741a68bc13e7726d7ac6d72
Guillaume Briard <guillaume.briard@gmail.com>
2022-04-08 13:31:41 +0700

    update du 08 04 2022

--- footer.php
+++ footer.php
@@ -18,11 +18,11 @@
 	<div id="sub-footer"><!-- #sub-footer -->
 		<div class="container">
 			<div class="row">
-				<div class="col-6 text-md-left contact">
+				<div class="col-sm-6 text-md-left contact">
 					<span><?php _e('Nous contacter', 'yachtsLyon_theme'); ?></span><br />
 					<a href="<?php echo get_site_url().'/contact/?lang='.ICL_LANGUAGE_CODE; ?>" class="customButton whiteButton"><?php _e('Un devis ?', 'yachtsLyon_theme'); ?> <i class="fas fa-chevron-right fa-xs"></i></a><br/>
 				</div>
-				<div class="col-6 text-md-left newsletter">
+				<div class="col-sm-6 text-md-left newsletter">
 					<?php if ( is_active_sidebar( 'sidebar-2' ) ) {
 						dynamic_sidebar( 'sidebar-2' );
 						}
@@ -37,9 +37,9 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-6 contactDetails">
-					<span><?php _e('Tél', 'yachtsLyon_theme'); ?> : 04 72 56 51 23<br />E-mail : <a href="mailto:contact@lesyachtsdelyon.com" target="_blank">contact@lesyachtsdelyon.com</a><br /><a href="https://www.lesyachtsdelyon.com/mesures-covid-19/">Mesures COVID-19</a></span>
+					<span><?php _e('Tél', 'yachtsLyon_theme'); ?> : 04 72 56 51 23<br />E-mail : <a href="mailto:contact@lesyachtsdelyon.com" target="_blank">contact@lesyachtsdelyon.com</a></span>
 				</div>
-				<div class="col-sm-6">
+				<div class="col-sm-6 sub-footer">
 					<div class="social">
 						<a href="https://www.facebook.com/pages/Les-Yachts-de-Lyon/215000789034" target="_blank">
 							<i class="fab fa-facebook-f fa-lg"></i>
@@ -49,6 +49,9 @@
 						</a>
 						<a href="https://twitter.com/yachtsdelyon" target="_blank">
 							<i class="fab fa-twitter fa-lg"></i>
+						</a>
+						<a href="https://www.linkedin.com/company/les-yachts-de-lyon-evenementiel/" target="_blank">
+							<i class="fab fa-linkedin-in fa-lg"></i>
 						</a>
 					</div>
 					<div class="copyright">
